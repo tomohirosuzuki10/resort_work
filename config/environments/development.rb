dde142f8ca54910e9de32ec5952742c3782ae094
tomohirosuzuki10 <tttttomo.1128@gmail.com>
2023-12-05 09:31:27 +0000

    ログイン登録エラー中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
+ 
+  config.hosts << "808d498078ca42c1af9f0d7463fc31c2.vfs.cloud9.ap-northeast-1.amazonaws.com"
+ 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
