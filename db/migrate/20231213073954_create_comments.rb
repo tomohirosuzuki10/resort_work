c865a738c57a8818a0c30436ae941b8fc3a12606
tomohirosuzuki <tttttomo.1128@gmail.com>
2024-01-08 20:48:43 +0900

    herokuデーターベース作成前

--- db/migrate/20231213073954_create_comments.rb
+++ db/migrate/20231213073954_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.text :comment_content
-      t.references :user, foreign_key: true
-      t.references :post, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20240108103836_change_datatype_post_id_of_comments.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypePostIdOfComments < ActiveRecord::Migration[6.1]
-  def change
-    change_column :comments, :post_id, :big
-  end
-end
