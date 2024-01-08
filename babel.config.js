b80b40bbb06d32e3266e1ea0bbf984c660ae1b5e
tomohirosuzuki <tttttomo.1128@gmail.com>
2024-01-08 16:36:38 +0900

    Update Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -60,12 +60,6 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-transform-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-transform-private-property-in-object',
         {
           loose: true
