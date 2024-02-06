a71b7e2e7309af56ad01661b5be3da081c8dcb3f
tomohirosuzuki <tttttomo.1128@gmail.com>
2024-02-06 17:29:00 +0900

    topデザイン完了

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,4 @@
+//= require jquery
+//= require activestorage
+//= require turbolinks
 //= require_tree .
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,70 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import ('jquery')
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(function(){
+	$(window).on('load scroll',function (){
+		$('.js-fade').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('scroll');
+			}
+		});
+	});
+});
+
+$(function(){
+	// 設定
+	var interval =4000; // 切り替わりの間隔（ミリ秒）
+	var fade_speed = 2000;// フェード処理の早さ（ミリ秒）
+	$(".top-concept-all img").hide();
+	$(".top-concept-all img:first").addClass("active").show();
+	
+	var changeImage = function(){
+			var $active = $(".top-concept-all img.active");
+			var $next = $active.next("img").length?$active.next("img"):$(".top-concept-all img:first");
+		 
+			$active.fadeOut(fade_speed).removeClass("active");
+			$next.fadeIn(fade_speed).addClass("active");
+	
+					}
+	
+	setInterval(changeImage,interval);
+	
+	}());
+
+	$(function(){
+		// 設定
+		var interval =4000; // 切り替わりの間隔（ミリ秒）
+		var fade_speed = 2000;// フェード処理の早さ（ミリ秒）
+		$(".top-conept-img2 img").hide();
+		$(".top-conept-img2 img:first").addClass("active").show();
+		
+		var changeImage = function(){
+				var $active = $(".top-conept-img2 img.active");
+				var $next = $active.next("img").length?$active.next("img"):$(".top-conept-img2 img:first");
+			 
+				$active.fadeOut(fade_speed).removeClass("active");
+				$next.fadeIn(fade_speed).addClass("active");
+		
+						}
+		
+		setInterval(changeImage,interval);
+		
+		}());
+		$('.nav_toggle').on('click', function () {
+			$('.nav_toggle, .nav-sp').toggleClass('show');
+		});
+		
